b58d0a6ee959ea2e575d776e19626f04d36e22a4
Abhijeet Deshmukh <abhijeetdeshmukh.2765@gmail.com>
2023-04-30 22:36:52 +0530

    git commit -m "Started with new project"

---
+++ app/src/main/java/com/vrundavan_technoking/stickynotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vrundavan_technoking.stickynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
